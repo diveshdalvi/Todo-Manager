bc5a67b0a621c794f54d6cad777b1e8191c5694c
diveshdalvi <dalvidivesh1@gmail.com>
2023-11-29 19:58:32 +0530

    work on task component

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import { createTheme } from '@mui/material';
 
-const Task = () => {
+const Task = ({taskName , taskTime}) => {
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main: '#DB0000',
+        light: '#DB0000',
+        dark : '#DB0000'
+      }
+    }
+
+  })
+
+  const time = new Date();
+  const [isChecked , setIsChecked] = useState(false);
+  const handleCheckBox = (e) => {
+    setIsChecked(e.target.checked);
+  }
+  const currentTime =time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  console.log(currentTime)
   return (
-    <div className=' text-gray-700 border-2 border-[#12486B]'>
-        Task
+    <div className='text-gray-700 flex justify-between mt-2'>
+      <Checkbox checked={isChecked} onChange={handleCheckBox}/>
+        <h2 className={isChecked?' mt-2 text-gray-400 font-semibold text-lg line-through':' mt-2 text-gray-600 font-semibold text-lg underline'}>This is Task</h2>
+        <p className={isChecked?' text-gray-400 mt-2.5 line-through mx-3':' text-gray-400 mt-2.5  mx-3'}>{currentTime}</p>
+        {isChecked?<IconButton  color ='primary' theme={theme} >
+          <DeleteIcon />
+        </IconButton>:<IconButton aria-label="delete" disabled color="primary">
+        <DeleteIcon />
+      </IconButton>}
+        
+        
     </div>
   )
 }
--- src/Components/TaskBox.jsx
+++ src/Components/TaskBox.jsx
@@ -2,19 +2,34 @@ import React from 'react'
 import { useState } from 'react';
 import { IoAddCircleSharp } from "react-icons/io5";
 import Task from './Task';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { createTheme } from '@mui/material';
 
 
 const TaskBox = () => {
+    const theme = createTheme({
+      palette:{
+        primary:{
+          main: '#295c7e',
+          light : '#12486B',
+          dark: '#152d3d'
+        }
+      }
+
+    })
+
     const currentDate = new Date().toJSON().slice(0,10);
   return (
     
     <div>
-      <div className=' bg-white flex rounded-xl flex-col p-7'>
+      <div className=' bg-white flex rounded-xl flex-col p-7 -mt-56'>
         <div className='mb-5'>
             <div className='flex justify-between'>
             <h2 className='text-gray-700 text-2xl font-bold mt-3 mr-5'>{currentDate}</h2>
-
-            <IoAddCircleSharp  style={{ color: "#12486B" }} size={'4rem'}className=' cursor-pointer tap-transparent'/>
+            <Fab   theme={theme} color='primary' aria-label="add">
+              <AddIcon />
+            </Fab>
             </div>
            <p className=' text-[#12486B] font-medium'>4 tasks</p>
         </div>
